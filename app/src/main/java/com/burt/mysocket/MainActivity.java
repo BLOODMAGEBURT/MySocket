6ae9e0978a70ac05ff995f33a7b661a4210af064
xujianbo <xujianbo@vocoor.com>
2019-07-09 18:24:17 +0800

    three button

--- app/src/main/java/com/burt/mysocket/MainActivity.java
+++ app/src/main/java/com/burt/mysocket/MainActivity.java
@@ -17,8 +17,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.ref.WeakReference;
-import java.lang.reflect.Array;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.nio.charset.Charset;
 import java.text.DecimalFormat;
 import java.util.Arrays;
@@ -26,7 +27,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String HOST = "192.168.1.81";//主机地址
     private static final int PORT = 9999;//端口号
@@ -42,9 +43,19 @@ public class MainActivity extends AppCompatActivity {
     int num = 90;
     int oldVal = 0;
     int newVal = 0;
+
+    private int status = 0;
+    private String Now = "@RUN_Now!";
+    private String Ready = "@Set_Mod3!";
+    private String Start = "@Set_Mod5!";
+
+
     private AppCompatButton toTest;
 
     private final MyHandler handler = new MyHandler(this);
+    private static AppCompatButton connect;
+    private AppCompatButton ready;
+    private AppCompatButton start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,10 +67,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        AppCompatButton send = findViewById(R.id.btn_send);
+        connect = findViewById(R.id.btn_con);
+        ready = findViewById(R.id.btn_ready);
+        start = findViewById(R.id.btn_start);
 
         toTest = findViewById(R.id.btn_test);
-
         waveView = findViewById(R.id.wave_progress);
         textProgress = findViewById(R.id.text_progress);
         waveView.setTextView(textProgress);
@@ -89,32 +101,172 @@ public class MainActivity extends AppCompatActivity {
         waveView.setProgressNum(oldVal, num, 1000);
         oldVal = num;
 
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("abc", "start");
+        connect.setOnClickListener(this);
+        ready.setOnClickListener(this);
+        start.setOnClickListener(this);
+        toTest.setOnClickListener(this);
+    }
 
-                mThreadPool.execute(new Runnable() {
-                    @Override
-                    public void run() {
 
-                        easyWay();
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_con:
+                connect.setText("连接中…");
+                connect();
 
-//                        complicateWay();
-                    }
-                });
-
-            }
-        });
-
-        toTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
+                break;
+            case R.id.btn_ready:
+                Toast.makeText(getApplicationContext(), "准备", Toast.LENGTH_SHORT).show();
+                toReady();
+                break;
+            case R.id.btn_start:
+                Toast.makeText(getApplicationContext(), "运行", Toast.LENGTH_SHORT).show();
+                toStart();
+                break;
+            case R.id.btn_test:
                 startActivity(new Intent(MainActivity.this, TestActivity.class));
+                break;
+        }
+    }
+
+
+    private void connect() {
+        mThreadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket();
+                    SocketAddress socAddress = new InetSocketAddress(HOST, PORT);
+                    socket.connect(socAddress, 3300);    //连接超时时间3秒3
+                    Message msg = handler.obtainMessage();
+                    msg.what = 0;
+                    Bundle bundle = new Bundle();
+                    bundle.putString("connectMsg", "连接成功");  //往Bundle中存放数据
+                    msg.setData(bundle);//mes利用Bundle传递数据
+                    handler.sendMessage(msg);
+
+                    socket.close();
+                } catch (IOException e) {
+                    Message msg = handler.obtainMessage();
+                    msg.what = 0;
+                    Bundle bundle = new Bundle();
+                    bundle.putString("connectMsg", "连接失败，请连接正确的WIFI");  //往Bundle中存放数据
+                    msg.setData(bundle);//mes利用Bundle传递数据
+                    handler.sendMessage(msg);
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void toReady() {
+        mThreadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    socket = new Socket(HOST, PORT);
+                    output = socket.getOutputStream();
+//            output.write((Now + "\n").getBytes("utf-8"));// 把msg信息写入输出流中
+                    output.write((Ready).getBytes("utf-8"));// 把msg信息写入输出流中
+                    output.flush();
+
+                    //--------接收服务端的返回信息-------------
+                    socket.shutdownOutput(); // 一定要加上这句，否则收不到来自服务器端的消息返回 ，意思就是结束msg信息的写入
+                    input = socket.getInputStream();
+                    byte[] b = new byte[1024];
+                    int len;
+                    sb = new StringBuffer();
+                    while ((len = input.read(b)) != -1) {
+                        sb.append(new String(b, 0, len, Charset.forName("UTF-8")));// 得到返回信息
+                    }
+                    Message msg = handler.obtainMessage();
+                    msg.what = 1;
+                    Bundle bundle = new Bundle();
+                    bundle.putString("readyMsg", sb.toString());  //往Bundle中存放数据
+                    msg.setData(bundle);//mes利用Bundle传递数据
+                    handler.sendMessage(msg);
+
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+    }
+
+    private void toStart() {
+        mThreadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    socket = new Socket(HOST, PORT);
+                    output = socket.getOutputStream();
+//            output.write((Now + "\n").getBytes("utf-8"));// 把msg信息写入输出流中
+                    output.write((Start).getBytes("utf-8"));// 把msg信息写入输出流中
+                    output.flush();
+
+                    //--------接收服务端的返回信息-------------
+                    socket.shutdownOutput(); // 一定要加上这句，否则收不到来自服务器端的消息返回 ，意思就是结束msg信息的写入
+                    input = socket.getInputStream();
+                    byte[] b = new byte[1024];
+                    int len;
+                    sb = new StringBuffer();
+                    while ((len = input.read(b)) != -1) {
+                        sb.append(new String(b, 0, len, Charset.forName("UTF-8")));// 得到返回信息
+                    }
+                    Message msg = handler.obtainMessage();
+                    msg.what = 2;
+                    Bundle bundle = new Bundle();
+                    bundle.putString("startMsg", sb.toString());  //往Bundle中存放数据
+                    msg.setData(bundle);//mes利用Bundle传递数据
+                    handler.sendMessage(msg);
+
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void getNow() {
+        mThreadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    socket = new Socket(HOST, PORT);
+                    output = socket.getOutputStream();
+//            output.write((Now + "\n").getBytes("utf-8"));// 把msg信息写入输出流中
+                    output.write((Now).getBytes("utf-8"));// 把msg信息写入输出流中
+                    output.flush();
+
+                    //--------接收服务端的返回信息-------------
+                    socket.shutdownOutput(); // 一定要加上这句，否则收不到来自服务器端的消息返回 ，意思就是结束msg信息的写入
+                    input = socket.getInputStream();
+                    byte[] b = new byte[1024];
+                    int len;
+                    sb = new StringBuffer();
+                    while ((len = input.read(b)) != -1) {
+                        sb.append(new String(b, 0, len, Charset.forName("UTF-8")));// 得到返回信息
+                    }
+                    Message msg = handler.obtainMessage();
+                    msg.what = 3;
+                    Bundle bundle = new Bundle();
+                    bundle.putString("nowMsg", sb.toString());  //往Bundle中存放数据
+                    msg.setData(bundle);//mes利用Bundle传递数据
+                    handler.sendMessage(msg);
+
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void complicateWay() {
@@ -134,40 +286,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void easyWay() {
-
-        try {
-
-            socket = new Socket(HOST, PORT);
-            output = socket.getOutputStream();
-            output.write(("are you ok ?" + "\n").getBytes("utf-8"));// 把msg信息写入输出流中
-            output.flush();
-
-            //--------接收服务端的返回信息-------------
-            socket.shutdownOutput(); // 一定要加上这句，否则收不到来自服务器端的消息返回 ，意思就是结束msg信息的写入
-            input = socket.getInputStream();
-            byte[] b = new byte[1024];
-            int len;
-            sb = new StringBuffer();
-            while ((len = input.read(b)) != -1) {
-                sb.append(new String(b, 0, len, Charset.forName("UTF-8")));// 得到返回信息
-            }
-            Message msg = handler.obtainMessage();
-            msg.what = 2;
-            Bundle bundle = new Bundle();
-            bundle.putString("receivedMsg", sb.toString());  //往Bundle中存放数据
-            msg.setData(bundle);//mes利用Bundle传递数据
-            handler.sendMessage(msg);
-
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-
-    static class MyHandler extends Handler {
+    class MyHandler extends Handler {
 
         float oldVal = 90;
         float newVal = 0;
@@ -187,25 +306,37 @@ public class MainActivity extends AppCompatActivity {
 
             switch (msg.what) {
                 case 0:
-                    Toast.makeText(activity, "连接成功", Toast.LENGTH_SHORT).show();
+                    String connectedMsg = msg.getData().getString("connectMsg");//接受msg传递过来的参数
+                    Toast.makeText(activity, connectedMsg, Toast.LENGTH_LONG).show();
+                    connect.setText("连接");
                     break;
                 case 1:
-                    Toast.makeText(activity, "连接断开", Toast.LENGTH_SHORT).show();
+                    String readyMsg = msg.getData().getString("readyMsg");//接受msg传递过来的参数
+                    Toast.makeText(activity, readyMsg, Toast.LENGTH_SHORT).show();
                     break;
                 case 2:
+                    String startMsg = msg.getData().getString("startMsg");//接受msg传递过来的参数
+                    Toast.makeText(activity, startMsg, Toast.LENGTH_SHORT).show();
 
-                    String receivedMsg = msg.getData().getString("receivedMsg");//接受msg传递过来的参数
+                    getNow();
 
+                    break;
+
+                case 3:
+                    String receivedMsg = msg.getData().getString("nowMsg");//接受msg传递过来的参数
+                    Log.d("abc", "receivedMsg:"+ receivedMsg);
                     int[] formatedMsg = formatTheMsg(receivedMsg.replace("{", "").replace("}", ""));
 
                     newVal = (float) (formatedMsg[2] - formatedMsg[3]) / (float) (formatedMsg[4] - formatedMsg[3]) * 100;
-                    activity.waveView.setProgressNum(oldVal, newVal, 5000);
+                    activity.waveView.setProgressNum(oldVal, newVal, 1000);
                     oldVal = newVal;
 
                     Toast.makeText(activity, Arrays.toString(formatedMsg), Toast.LENGTH_SHORT).show();
                     break;
             }
         }
+
+
 
         private int[] formatTheMsg(String receivedMsg) {
 
@@ -223,7 +354,6 @@ public class MainActivity extends AppCompatActivity {
             return intTemp;
         }
     }
-
 
     @Override
     protected void onDestroy() {
